2072b19fffd56138d87d83dcb764a8b7180c7502
Alberto Parolari Blasi <apb.freelance@gmail.com>
2025-04-08 15:27:58 +0200

    update bootloader.c

--- boot/bootloader.c
+++ boot/bootloader.c
@@ -5,7 +5,7 @@ __attribute__((section(".start")))
 extern void print_char(char c);  // Definida en ASM
 
 void boot_main(void) {
-    const char *msg1 = "[INT10h] Hola desde BIOS!\r\n";
+    const char *msg1 = "Hola desde BIOS! camino de Stage2...\r\n";
 
     // Imprimir usando rutina externa ASM (int 10h)
     for (int i = 0; msg1[i] != '\0'; i++) {
@@ -16,7 +16,7 @@ void boot_main(void) {
 
 // Espacio de relleno hasta byte 510
 __attribute__((section(".fill")))
-char filler[510 - 2 - 126] = {0};
+char filler[510 - 2 - 158] = {0};
 //const unsigned char padding[510] = {0};
 
 // Firma obligatoria al final del sector
